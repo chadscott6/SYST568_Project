f515c60243b2276f33d5f2fb1d0dc99814d14306
Michael Gasvoda <mgasvoda@gmail.com>
2020-12-03 11:08:22 -0500

    including all standardized variables

--- scripts/clean_data.R
+++ scripts/clean_data.R
@@ -90,14 +90,14 @@
   # names(Teams_w_salary)[c(15:28,30:39)] # list of columns that are being standardized
   setDT(Teams_w_salary)[, paste0(names(Teams_w_salary)[c(15:28,30:39)],"_G") :=
                            lapply(.SD, '/', Teams_w_salary$G), .SDcols = c(15:28,30:39)]
-  Teams_w_salary
+  # Teams_w_salary
   
 
 ### Features for modeling ##########################################
 
 ### Data with salary ### 
   ## Need to Change this
-  final_teams_salary <- Teams_w_salary[,c(1,6,15,17:24,28,33:36,50,51)]
+  final_teams_salary <- Teams_w_salary[,c(1,6,15:28,30:39,50,51)]
 
 
 # write out to csv
