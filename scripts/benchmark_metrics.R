e4b4041c7aab20fd7bf940324869e7cbf60715fa
Jonathan Nelson <jrnecon16@gmail.com>
2020-12-03 12:25:03 -0500

    benchmark metrics adjustment

--- scripts/benchmark_metrics.R
+++ scripts/benchmark_metrics.R
@@ -6,23 +6,22 @@ playoffs_year <- final_teams_salary %>%
   count(playoff_nextyear) %>%
   filter(playoff_nextyear == 'Y')
 playoffs_year <- playoffs_year[,c(1,3)]
+# get total number of teams per year
+teams_year <- final_teams_salary %>%
+  group_by(yearID) %>%
+  count()
 # get random results
-total_true = vector()
-total_pred = vector()
-for (year in seq(1985,2018,1)) {
-  if (year == 1993 ){
-    next
-  }
+total_random_pred = vector()
+set.seed(1985)
+for (year in unique(final_teams_salary$yearID)) {
   n <- filter(playoffs_year, yearID==year)$n
-  set.seed(1)
-  random_true = sample(c(rep(0,30-n),rep(1,n)))
-  set.seed(2)
-  random_pred = sample(c(rep(0,30-n),rep(1,n)))
-  total_true = append(total_true, random_true)
-  total_pred = append(total_pred, random_pred)
+  teams <- filter(teams_year, yearID==year)$n
+  set.seed(year)
+  random_pred = sample(c(rep('N',teams-n),rep('Y',n)))
+  total_random_pred = append(total_random_pred, random_pred)
 }
 # produce confusion matrix
-bm.ConMatrix = table(total_true, total_pred)
+bm.ConMatrix = table(final_teams_salary$playoff_nextyear, total_random_pred)
 print(bm.ConMatrix)
 # calculate metrics
 bm.accuracy = (bm.ConMatrix[1, 1] + bm.ConMatrix[2, 2]) /  sum(bm.ConMatrix)
@@ -31,5 +30,5 @@ bm.recall = bm.ConMatrix[2, 2] / (bm.ConMatrix[2, 2] + bm.ConMatrix[2, 1])
 bm.f1score = 2*(bm.precision*bm.recall / (bm.precision+bm.recall))
 print(bm.accuracy)
 print(bm.f1score)
-# benchmark accuracy score is approx. 0.634
-# benchmark f1 score is approx. 0.267
+# benchmark accuracy score is approx. 0.622
+# benchmark f1 score is approx. 0.281
